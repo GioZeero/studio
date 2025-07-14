d890c29a307e0cc1ef9ca7e66dc2e727992e7b05
Giovanni Storelli <Giovanni.storelli2001@gmail.com>
2025-07-14 12:59:09 +0000

    Aggiungi la possibilità che il sito si possa installare sul dispositivo

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    const { NormalModuleReplacementPlugin } = require('webpack');
+    // a bug in `node-fetch` required by `firebase-admin` causes a build-time error, so we need to mock it
+    // https://github.com/firebase/firebase-admin-node/issues/1471
+    // https://github.com/node-fetch/node-fetch/issues/784
+    config.plugins.push(
+      new NormalModuleReplacementPlugin(
+        /^node-fetch$/,
+        require.resolve('./node-fetch.mock.js')
+      )
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 export const metadata: Metadata = {
   title: 'GymAgenda',
   description: 'App per la prenotazione di orari in palestra',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <ThemeProvider
